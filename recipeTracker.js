9f852a034b020ff803c9079dd5631385d01860ac
Ayanda Mdoko <119368717+AyandaMdoko@users.noreply.github.com>
2026-07-02 21:01:14 +0200

    first commit

--- recipeTracker.js
+++ recipeTracker.js
@@ -24,11 +24,40 @@ const recipe3 = {
   difficultyLevel: ""
 };
 
-const recipe1Name = recipe1.name;
-console.log(recipe1Name);
+ recipes.push(recipe1, recipe2, recipe3);
 
-const recipe2CookingTime = recipe2.cookingTime;
-console.log(recipe2CookingTime);
+console.log(recipes);
 
-const recipe3Ingredients = recipe3.ingredients;
-console.log(recipe3Ingredients);    
+
+function getTotalIngredients(ingredients) {
+  return ingredients.length;
+}
+
+function getDifficultyLevel(cookingTime) {
+  if (cookingTime <= 30) {
+    return "easy";
+  }
+
+  if (cookingTime <= 60) {
+    return "medium";
+  }
+
+  return "hard";
+}
+
+const recipe1TotalIngredients = getTotalIngredients(recipe1.ingredients);
+console.log(recipe1TotalIngredients);
+
+const recipe1DifficultyLevel = getDifficultyLevel(recipe1.cookingTime);
+console.log(recipe1DifficultyLevel);
+
+recipe1.totalIngredients = getTotalIngredients(recipe1.ingredients);
+recipe1.difficultyLevel = getDifficultyLevel(recipe1.cookingTime);
+
+recipe2.totalIngredients = getTotalIngredients(recipe2.ingredients);
+recipe2.difficultyLevel = getDifficultyLevel(recipe2.cookingTime);
+
+recipe3.totalIngredients = getTotalIngredients(recipe3.ingredients);
+recipe3.difficultyLevel = getDifficultyLevel(recipe3.cookingTime);
+
+console.log(recipes);
