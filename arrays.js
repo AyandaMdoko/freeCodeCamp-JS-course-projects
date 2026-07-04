0dafa1ce0e319eb02593913f01bf06b723aad759
Ayanda Mdoko <119368717+AyandaMdoko@users.noreply.github.com>
2026-07-04 13:25:30 +0200

    forst commit

--- arrays.js
+++ arrays.js
@@ -123,3 +123,19 @@ console.log(fruits);
 
 const numbers = [1, 2, 3];
 console.log(numbers[10]);
+
+function updateRecords(records, id, prop, value) {
+  if (value === "") {
+    delete records[id][prop];
+  } else if (prop !== "tracks") {
+    records[id][prop] = value;
+  } else {
+    if (!records[id].hasOwnProperty("tracks")) {
+      records[id].tracks = [];
+    }
+
+    records[id].tracks.push(value);
+  }
+
+  return records;
+}
