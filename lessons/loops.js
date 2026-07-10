d01d65abf3c4385610fc0fced4eb9889d7e65450
Ayanda Mdoko <119368717+AyandaMdoko@users.noreply.github.com>
2026-07-10 17:30:27 +0200

    Update loops.js

--- lessons/loops.js
+++ lessons/loops.js
@@ -8,12 +8,19 @@
 //   console.log(num);
 // }
 
-const people = [
-  { name: 'John', age: 30 },
-  { name: 'Jane', age: 25 },
-  { name: 'Jim', age: 40 }
-];
 
-for (const person of people) {
-  console.log(`${person.name} is ${person.age} years old`);
+// const people = [
+//   { name: 'John', age: 30 },
+//   { name: 'Jane', age: 25 },
+//   { name: 'Jim', age: 40 }
+// ];
+
+// for (const person of people) {
+//   console.log(`${person.name} is ${person.age} years old`);
+// }
+
+const fruits = ['apple', 'banana', 'cherry'];
+
+for (let fruit of fruits) {
+  console.log(fruit.toUpperCase());
 }
