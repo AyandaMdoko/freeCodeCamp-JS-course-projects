713ccbd249335a21e334b0b19a94f43f9a3ce577
Ayanda Mdoko <119368717+AyandaMdoko@users.noreply.github.com>
2026-07-11 16:46:46 +0200

    Update loops.js

--- lessons/loops.js
+++ lessons/loops.js
@@ -19,8 +19,72 @@
 //   console.log(`${person.name} is ${person.age} years old`);
 // }
 
-const fruits = ['apple', 'banana', 'cherry'];
+// const fruits = ['apple', 'banana', 'cherry'];
 
-for (let fruit of fruits) {
-  console.log(fruit.toUpperCase());
-}
+// for (let fruit of fruits) {
+//   console.log(fruit.toUpperCase());
+// }
+
+// const fruit = {
+//   name: 'apple',
+//   color: 'red',
+//   price: 0.99
+// };
+
+// for (const prop in fruit) {
+//   console.log(fruit[prop]);
+// }
+
+// const person = {
+//   name: 'John',
+//   age: 30,
+//   address: {
+//     street: '123 Main St',
+//     city: 'Anytown',
+//     state: 'CA'
+//   }
+// };
+
+// for (const prop in person) {
+//   console.log(person[prop]);
+// }
+
+// const person = {
+//   name: 'John',
+//   age: 30,
+//   address: {
+//     street: '123 Main St',
+//     city: 'Anytown',
+//     state: 'CA'
+//   }
+// };
+
+// function isObject(obj) {
+//   return typeof obj === 'object' && !Array.isArray(obj) && obj !== null;
+// }
+
+// for (const prop in person) {
+//   if (isObject(person[prop])) {
+//     for (const nestedProp in person[prop]) {
+//       console.log(person[prop][nestedProp]);
+//     }
+//   } else {
+//     console.log(person[prop]);
+//   }
+// }
+
+// while loops
+
+// let counter = 0;
+// while(counter < 5) {
+//   console.log(counter);
+//   counter++;
+// }
+
+//do while loops
+
+let counter = 0;
+do {
+  console.log(counter);
+  counter++;
+} while (counter < 10); 
