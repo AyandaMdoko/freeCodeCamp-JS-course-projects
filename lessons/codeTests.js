6761c744d62cc68b0f490122ff17a41b0eaa696f
AyandaMdoko <ayandamdoko665@gmail.com>
2026-08-19 12:23:49 +0200

    Create codeTests.js

---
+++ lessons/codeTests.js
@@ -0,0 +1,29 @@
+// let str = "JavaScript";
+// let objStr = new String("JavaScript");
+// console.log(str.length === objStr.length);
+
+// const num = 10;
+// console.log(num.toString(2)); // "1010"
+
+// const person = {
+//   name: "John",
+//   age: 30,
+//   isStudent: true
+// };
+
+// console.log(person.toString()); // "[object Object]"
+
+// let num = 5.7;
+// console.log(num.toString());
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.toString());
+
+// let obj = { name: "John", age: 30 };
+// console.log(obj.toString());
+
+const boolTrue = Number(true);
+const boolFalse = Number(false);
+
+console.log(boolTrue);
+consol.log(boolFalse);
