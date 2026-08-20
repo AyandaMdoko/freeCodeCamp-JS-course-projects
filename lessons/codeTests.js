5ab07457a6abe37baed07e078ce0b98ca3afd2be
AyandaMdoko <ayandamdoko665@gmail.com>
2026-08-20 14:39:52 +0200

    Update codeTests.js

--- lessons/codeTests.js
+++ lessons/codeTests.js
@@ -22,8 +22,49 @@
 // let obj = { name: "John", age: 30 };
 // console.log(obj.toString());
 
-const boolTrue = Number(true);
-const boolFalse = Number(false);
+// const boolTrue = Number(1);
+// const boolFalse = Number(0);
 
-console.log(boolTrue);
-consol.log(boolFalse);
+// console.log(boolTrue);
+// console.log(boolFalse);
+
+// const emptyArr = Number([]);
+// const arrOneNum = Number([7]);
+// const arrMultiNum = Number([7, 36, 12]);
+// const arrStr = Number(["str1"]);
+// const arrMultiStr = Number(["str1", "str2"]);
+
+// console.log(emptyArr); // 0
+// console.log(arrOneNum); // 7
+// console.log(arrMultiNum); // NaN
+// console.log(arrStr); // NaN
+// console.log(arrMultiStr); // NaN
+
+// const emptyArray = new Array(5);
+// console.log(emptyArray.length); // 5
+// console.log(emptyArray); // [ , , , , ]
+
+// const fixedLengthArray = Array.from({ length: 5 });
+// console.log(fixedLengthArray.length); // 5
+// console.log(fixedLengthArray); // [undefined, undefined, undefined, undefined, undefined]
+
+function createCounter() {
+    let count = 0;
+    return function () {
+        count++;
+        return count;
+    };
+}
+
+let counter = createCounter();
+console.log(counter()); // 1
+console.log(counter()); // 2
+
+function outer(x) {
+    return function(y) {
+        return x + y;
+    };
+}
+
+let add5 = outer(5);
+console.log(add5(3));
