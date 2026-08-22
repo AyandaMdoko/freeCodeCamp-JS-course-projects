741bf0a984d3df2f5ca4e3d16c0468c617d0ac79
AyandaMdoko <ayandamdoko665@gmail.com>
2026-08-22 13:00:53 +0200

    Create JSFundamentalsReview.js

---
+++ lessons/JSFundamentalsReview.js
@@ -0,0 +1,37 @@
+// Function to reverse a string
+// function reverseString(str){
+//   let reversed = "";
+//   for (let i = str.length - 1; i >= 0; i--){
+//     reversed += str[i];
+//   }
+//   return reversed;
+// }
+
+// console.log(reverseString("hello")); // "olleh"
+
+// Function to check if a string is a palindrome
+function largestOfAll(arr){
+  let largest = [];
+
+  for (let i = 0; i < arr.length; i++){
+    largest.push(Math.max(...arr[i]));
+  }
+  return largest;
+}
+
+console.log(largestOfAll([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+// [5, 27, 39, 1001]
+
+//function that returns the first element that passes a test function
+function findElement(arr, func) {
+  for (let i = 0; i < arr.length; i++) {
+    if (func(arr[i])) {
+      return arr[i];
+    }
+  }
+  return undefined;
+}
+
+// Example usage:
+console.log(findElement([1, 2, 3, 4], num => num % 2 === 0)); // 2
+console.log(findElement([1, 3, 5, 9], num => num % 2 === 0)); // undefined
